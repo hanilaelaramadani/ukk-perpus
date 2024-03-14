3a08d9b871b0032969b8bbdc0df41e8d7525ff11
hanilaelaramadani <hanilaela516@gmail.com>
2024-03-14 13:34:10 +0700

    update welcome

--- resources/views/buku/detail.blade.php
+++ resources/views/buku/detail.blade.php
@@ -1,17 +1,39 @@
-<div class="container">
-        <div class="row">
-            
-            <div class="col-lg-3 mb-3 d-flex align-items-stretch"> <!-- Adjust the column size based on your preference -->
-                <div class="card flex-grow-1">
-                    <img src="{{ asset('storage/'.$buku->foto) }}" class="card-img-top" alt="">
-                    <div class="card-body d-flex flex-column ">
-                        <h5 class="card-title">{{$buku->judul}}</h5>
-                        <p class="card-text">{{$buku->penulis}}</p>
-                        <p class="card-text">{{$buku->penerbit}}</p>
-                        <p class="card-text">{{$buku->tahun_terbit}}</p>
+<!DOCTYPE html>
+<html lang="en"> 
+       
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Perpustakaan Digital</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-4"> <!-- Adjust the column size based on your preference -->
+                <div class="card mb-4">
+                <img src="{{ asset('storage/'.$buku->foto) }}" class="rounded" alt="...">
+                    <div class="card-body">
+                        <table class="table table-hover">
+                            <tr>
+                                <th >Judul Buku: </th>
+                                <td >{{$buku->judul}}</td>
+                            </tr>
+                            <tr>
+                                <th >Penulis: </th>
+                                <td >{{$buku->penulis}}</td>
+                            </tr>
+                            <tr>
+                                <th >Penerbit: </th>
+                                <td >{{$buku->penerbit}}</td>
+                            </tr>
+                            <tr>
+                                <th >Tahun Terbit: </th>
+                                <td >{{$buku->tahun_terbit}}</td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
             </div>
-            
         </div>
     </div>
